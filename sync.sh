c79a8d5560e7088a15bc83d44e720b60df41fcff
AnGgIt086 <jarbull86@gmail.com>
2025-03-13 21:28:28 +0700

    Testss

--- sync.sh
+++ sync.sh
@@ -10,42 +10,42 @@ gh auth login --with-token < ikitoken.txt
 git clone --single-branch "${FOX_SYNC}" "${CIRRUS_WORKING_DIR}"/Orangefox/sync --depth=1
 cd "${CIRRUS_WORKING_DIR}"/Orangefox/sync
 ./orangefox_sync.sh --branch "${FOX_SYNC_BRANCH}" --path "${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}"
-if [[ ! -d bootable/recovery/gui/theme ]]; then
-    echo -e ${cya} "Adding Theme..."
-    git clone https://gitlab.com/OrangeFox/misc/theme.git bootable/recovery/gui/theme
+if [[ ! -d ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/bootable/recovery/gui/theme ]]; then
+    echo "Adding Theme..."
+    git clone https://gitlab.com/OrangeFox/misc/theme.git ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/bootable/recovery/gui/theme
 fi
-if [[ ! -d external/nano ]]; then
-    echo -e ${cya} "Adding Nano Library..."
-    git clone https://github.com/LineageOS/android_external_nano -b lineage-19.1 external/nano
+if [[ ! -d ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/external/nano ]]; then
+    echo "Adding Nano Library..."
+    git clone https://github.com/LineageOS/android_external_nano -b lineage-19.1 ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/external/nano
 fi
-if [[ ! -d external/libncurses ]]; then
-    echo -e ${cya} "Adding libncurses Library..."
-    git clone https://github.com/LineageOS/android_external_libncurses -b lineage-19.1 external/libncurses
+if [[ ! -d ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/external/libncurses ]]; then
+    echo "Adding libncurses Library..."
+    git clone https://github.com/LineageOS/android_external_libncurses -b lineage-19.1 ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/external/libncurses
 fi
-if [[ ! -d external/bash ]]; then
-    echo -e ${cya} "Adding Bash Library..."
-    git clone https://github.com/LineageOS/android_external_bash -b lineage-19.1 external/bash
+if [[ ! -d ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/external/bash ]]; then
+    echo "Adding Bash Library..."
+    git clone https://github.com/LineageOS/android_external_bash -b lineage-19.1 ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/external/bash
 fi
-if [[ ! -d external/lptools ]]; then
-    echo -e ${cya} "Adding lptools Library..."
-    git clone https://github.com/phhusson/vendor_lptools external/lptools
+if [[ ! -d ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/external/lptools ]]; then
+    echo "Adding lptools Library..."
+    git clone https://github.com/phhusson/vendor_lptools ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/external/lptools
 fi
 if [[ ! -z "${MAINTAINER_URL}" ]]; then
-    echo -e ${cya} "Change Maintainer Profile..."
+    echo "Change Maintainer Profile..."
     wget "${MAINTAINER_URL}" -O "${CIRRUS_WORKING_DIR}"/maintainer.png
-    cp -r "${CIRRUS_WORKING_DIR}"/maintainer.png bootable/recovery/gui/theme/portrait_hdpi/images/Default/About
+    cp -r "${CIRRUS_WORKING_DIR}"/maintainer.png ${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/bootable/recovery/gui/theme/portrait_hdpi/images/Default/About
 fi
 if [[ ! -z "${KERNEL_TREE}" ]]; then
-    echo -e ${cya} "Clonning Kernel Tree..."
-    git clone "${KERNEL_TREE}" -b "${KERNEL_BRANCH}" "${KERNEL_PATH}"
+    echo "Clonning Kernel Tree..."
+    git clone "${KERNEL_TREE}" -b "${KERNEL_BRANCH}" "${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/${KERNEL_PATH}"
 fi
 if [[ ! -z "${COMMON_TREE}" ]]; then
-    echo -e ${cya} "Clonning Common Tree..."
-    git clone "${COMMON_TREE}" -b "${COMMON_BRANCH}" "${COMMON_PATH}"
+    echo "Clonning Common Tree..."
+    git clone "${COMMON_TREE}" -b "${COMMON_BRANCH}" "${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/${COMMON_PATH}"
 fi
 if [[ ! -z "${DEVICE_TREE}" ]]; then
-    echo -e ${cya} "Clonning Device Tree..."
-    git clone "${DEVICE_TREE}" -b "${DEVICE_TREE_BRANCH}" "${DEVICE_PATH}"
+    echo "Clonning Device Tree..."
+    git clone "${DEVICE_TREE}" -b "${DEVICE_TREE_BRANCH}" "${CIRRUS_WORKING_DIR}"/Orangefox/fox_"${FOX_SYNC_BRANCH}/${DEVICE_PATH}"
 fi
 
 exit 0
